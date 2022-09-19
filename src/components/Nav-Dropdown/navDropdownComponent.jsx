b8179f8869d4a41cf4eb1306ee1fb4c3a7d826ad
codewarsfx <chiderainnocent001@gmail.com>
2022-09-19 19:57:15 +0100

    refactored the Navigation component

--- src/components/Nav-Dropdown/navDropdownComponent.jsx
+++ src/components/Nav-Dropdown/navDropdownComponent.jsx
@@ -1,8 +1,87 @@
-const NavDropdown = ({click}) =>{
+import styled from "styled-components"
 
-r
 
+import useModal from "../../Hooks/useModal"
+import AnimatedModal from "../animatedModal/animatedModal"
+import CenterContainer from "../CenterPop/centerPopComponent"
+
+
+const NavDropdown = () => {
+
+const {showModal,click} = useModal()
+
+return (
+    <DropdownContainer>
+        <DropdownTop>
+        <DropdownList>
+            <li>
+            <span onClick={click} className="navbutton-dropdown-link navbutton-dropdown-link--bold">Sign Up</span>
+            </li>
+            <li>
+            <span  className="navbutton-dropdown-link">Log in</span>
+            </li>
+        </DropdownList>
+        </DropdownTop>
+        <div>
+        <DropdownList>
+            <li>
+            <span className="navbutton-dropdown-link">Host your home</span>
+            </li>
+            <li>
+            <span className="navbutton-dropdown-link">Host an experience</span>
+            </li>
+            <li>
+            <span  className="navbutton-dropdown-link">Help</span>
+            </li>
+        </DropdownList>
+        </div>
+        <AnimatedModal showModal={showModal} click={click} component={<CenterContainer/>}/>
+    </DropdownContainer>
+)
 }
+
+const DropdownContainer = styled.div`
+    position: absolute;
+    top: 120%;
+    font-size: 13px;
+    width: 94%;
+    left: 50%;
+    margin-left: -45%;
+    box-shadow: .1px 0px 30px -10px rgb(176, 176, 176);
+    background-color: #fff;
+    border: .5px solid var(--light-grey);
+    border-radius: 10px;
+    
+`
+
+const DropdownTop = styled.div`
+border-bottom: 1px solid var(--light-grey);
+    
+`
+
+
+
+const DropdownList = styled.ul`
+    margin: 0;
+    padding: .8em 0;
+    list-style-type: none;
+
+    .navbutton-dropdown-link{
+        color: var(--text-color-dark);
+        text-decoration: none;
+        padding:  1em;
+        display: block;
+
+    :hover{
+      background-color: var(--background-hover);
+     }
+    }
+
+    .navbutton-dropdown-link--bold{
+        font-weight: 600;
+    }
+
+`
 
 
 export default NavDropdown
--- src/components/Nav/NavComponent.jsx
+++ src/components/Nav/NavComponent.jsx
@@ -4,16 +4,11 @@ import { useState } from "react"
 
 
 import "./nav.css"
-
-import AnimatedModal from "../animatedModal/animatedModal"
-import CenterContainer from "../CenterPop/centerPopComponent"
-import useModal from "../../Hooks/useModal"
-
+import NavDropdown from "../Nav-Dropdown/navDropdownComponent"
 
 
 const Navigation =()=>{
    const [showDropdown, setShowDropdown] = useState(false)
-    const {showModal,click} = useModal()
 
     return(
       <Container>
@@ -27,34 +22,8 @@ const Navigation =()=>{
         <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{display: "block", fill: "none",height: "16px",width: "16px", stroke: "#222222", strokeWidth: "3", overflow: "visible"}}><g fill="none" fillRule="nonzero"><path d="m2 16h28"></path><path d="m2 24h28"></path><path d="m2 8h28"></path></g></svg>
         <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{display: "block", height: "30px", width: "30px", fill: "currentcolor"}}><path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path></svg>
         </NavButton>
-       {showDropdown &&  <div className="navbutton-dropdown">
-           <div className="navbutton-dropdown-top">
-             <ul className="navbutton-dropdown-list">
-                <li>
-                  <span onClick={click} className="navbutton-dropdown-link navbutton-dropdown-link--bold">Sign Up</span>
-                </li>
-                <li>
-                  <span  className="navbutton-dropdown-link">Log in</span>
-                </li>
-             </ul>
-           </div>
-           <div className="navbutton-dropdown-bottom">
-           <ul className="navbutton-dropdown-list">
-                 <li>
-                  <span className="navbutton-dropdown-link">Host your home</span>
-                </li>
-                <li>
-                  <span className="navbutton-dropdown-link">Host an experience</span>
-                </li>
-                <li>
-                  <span  className="navbutton-dropdown-link">Help</span>
-                </li>
-            </ul>
-           </div>
-           <AnimatedModal showModal={showModal} click={click} component={<CenterContainer/>}/>
-        </div>
+       {showDropdown && <NavDropdown/>
         }
-     
       </Container>
     )
 }
@@ -64,7 +33,7 @@ const Container = styled.nav`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    width: 253px;
+    width: 260px;
     position: relative;
     @media (max-width: 750px) {
     display: none;
