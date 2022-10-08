ff0233b1cd6e467f69a7edb10479bd8d5c27feae
codewarsfx <chiderainnocent001@gmail.com>
2022-10-08 22:20:50 +0100

    details description section mobile screen

--- src/pages/AirbnbDetails/airbnbdetails.jsx
+++ src/pages/AirbnbDetails/airbnbdetails.jsx
@@ -1,16 +1,47 @@
+import { AiFillStar } from 'react-icons/ai'
+import {GiGhostAlly} from 'react-icons/gi'
+
 import './airbnbdetails.css'
 
 
 import HomeDetailsHeader from '../../components/HomeDetailsHeader/homeDetailsHeader'
 import HomeHero from '../../components/homeDetailsHero/homeDetailsHero'
+
+
 import './airbnbdetails.css'
 const AirbBnbDetails = ()=>{
     return (
    <div className="details-container">
     <HomeDetailsHeader/>
     <HomeHero/>
-   </div>
+    <div className="details-description">
+            <h1 className="details-description-title">
+                Hacienda el Terrero - Casa Los Enanos
+            </h1>
+            
+            <p className="details-description-text">
 
+         
+            <span className="details-description-stats">
+            <AiFillStar className='details-description-stats-icon'/> 
+            5.0
+            </span>
+            <span className="details-description-seperation"></span>
+            <span className="details-description-stats details-description-stats-primary">
+            18 reviews 
+            </span>
+            <span className="details-description-seperation"></span>
+            <span className="details-description-stats">
+            <GiGhostAlly className='details-description-stats-icon' />
+            Superhost 
+            </span>
+            <span className="details-description-seperation"></span>
+            <span className="details-description-stats details-description-stats-primary">
+                Los Realejos,Canarias,Spain
+            </span>
+            </p>
+    </div>
+   </div>
     )
 }
 
